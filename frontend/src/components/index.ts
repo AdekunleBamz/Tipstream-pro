983b070a6247e353e921d82c26146a289997380c
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-02 16:16:35 +0100

    feat: add components barrel export file

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,8 @@
+export { Navbar } from "./Navbar";
+export { ChainGuard } from "./ChainGuard";
+export { TipForm } from "./TipForm";
+export { DailyCheckInCard } from "./DailyCheckInCard";
+export { SubscriptionForm } from "./SubscriptionForm";
+export { NFTGallery } from "./NFTGallery";
+export { HeroSection } from "./HeroSection";
+export { StatsCards } from "./StatsCards";
