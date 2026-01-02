d1b4a12bf90643f5f817a232514429e0b2e9da84
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-02 16:11:00 +0100

    feat: add ABI barrel export file

---
+++ frontend/src/config/abis/index.ts
@@ -0,0 +1,4 @@
+export { TipStreamABI } from "./TipStreamABI";
+export { SubscriptionManagerABI } from "./SubscriptionManagerABI";
+export { TipNFTABI } from "./TipNFTABI";
+export { DailyCheckInABI } from "./DailyCheckInABI";
